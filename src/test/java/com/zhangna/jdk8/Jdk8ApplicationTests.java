57f9839214a7dce1f11072e93ad9ba9eb1c21211
zhangna <zhangna@zto.cn>
2020-08-27 18:07:40 +0800

    提交java8的源码阅读笔记

---
+++ src/test/java/com/zhangna/jdk8/Jdk8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangna.jdk8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
